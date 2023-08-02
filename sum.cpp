691ca4a0a4733e9003f2d91dbda892e3cb2f7489
Priyanshu soni <priyanshus190102@gmail.com>
2023-08-02 10:44:34 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+#define ll long long
+#include <math.h>
+#include <algorithm>
+
+int main(){
+    int a=1;
+    int b=6;
+
+    cout<<(a+b);
+}
